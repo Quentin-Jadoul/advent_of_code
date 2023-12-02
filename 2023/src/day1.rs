514487ed89273d867258f3bb5e446286c9b13155
Quentin-Jadoul <18155@ecam.be>
2023-12-02 15:45:04 +0100

    day1 cleaned

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -3,13 +3,27 @@ use crate::input;
 pub fn day1() -> input::Result<()> {
     let contents = input::load_day_file("day1.txt");
 
-    let mut sum = 0;
+    println!("Part 1: {}", part1(&contents));
+    println!("Part 2: {}", part2(&contents));
+    
+    Ok(())
+}
 
-    // create a dictionary of numbers and their corresponding words
+pub fn part1(contents: &String) -> usize {
+    let dict: std::collections::HashMap<&str, usize> = 
+        [("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)].iter().cloned().collect();
+    count(&dict, &contents)
+}
+
+pub fn part2(contents: &String) -> usize {
     let dict: std::collections::HashMap<&str, usize> = 
         [("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9), ("1", 1), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9)].iter().cloned().collect();
-    // create a dictionary of a number and its position in the line, initialize it empty
-    
+    count(&dict, &contents)
+}
+
+pub fn count(dict: &std::collections::HashMap<&str, usize>, contents: &String) -> usize {
+    let mut sum = 0;
+
     for line in contents.lines() {
         let mut pos: std::collections::HashMap<usize, usize> = std::collections::HashMap::new();
         for word in dict.keys() {
@@ -19,8 +33,6 @@ pub fn day1() -> input::Result<()> {
                 start += index + word.len();
             }
         }
-        // print the dictionary
-        println!("{:?}", pos);
 
         // in the pos dictionary, keep only the smallest and the biggest key (position)
         let first: &usize = pos.iter().min_by_key(|x| x.0).unwrap().1;
@@ -28,13 +40,9 @@ pub fn day1() -> input::Result<()> {
 
         // concatenate the first and last number
         let num = format!("{}{}", first, last).parse::<usize>().unwrap();
-        println!("{}", num);
 
         sum += num;
     }
-
-    println!("Part 1: {}", sum);
-    
-    Ok(())
+    sum 
 }
 
