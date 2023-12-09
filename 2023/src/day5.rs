e410b3fe25fe8a5df609fc51a2352bd4eaa664cb
Quentin-Jadoul <18155@ecam.be>
2023-12-09 23:50:12 +0100

    Day 5 part_1 opti

--- 2023/src/day5.rs
+++ 2023/src/day5.rs
@@ -1,4 +1,5 @@
 use crate:: input;
+use std::collections::HashMap;
 
 pub fn day5() -> input::Result<()> {
     println!("Part 1: {}", part_1());
@@ -13,33 +14,44 @@ pub fn part_1() -> usize {
     // Split the content on empty lines
     let groups: Vec<&str> = content.split("\n\n").collect();
 
-    // Get the first group
+    // Get the first group and parse it into seeds
     let seeds: Vec<usize> = groups[0].split_whitespace()
         .filter_map(|s| s.parse::<usize>().ok())
         .collect();
 
-    // For each group, get the group and the index
-    let mut location: Vec<usize> = Vec::new();
-    for seed in seeds.iter() {
-        let mut source = *seed;
-        for (_i, group) in groups.iter().enumerate().skip(1) {
-            for line in group.lines().skip(1) {
-                let numbers: Vec<usize> = line.split_whitespace()
-                    .filter_map(|s| s.parse().ok())
-                    .collect();
-                let (dest_range_start, source_range_start, range_length) = (numbers[0], numbers[1], numbers[2]);
-                if (&source >= &source_range_start) && (&source <= &(source_range_start + range_length)) {
-                    let offset: isize = dest_range_start as isize - source_range_start as isize;
-                    source = (source as isize + offset) as usize;
-                    break;
-                }
+    // Parse the rest of the groups into maps
+    let maps: Vec<HashMap<usize, (usize, usize)>> = groups[1..].iter().map(|group| {
+        group.lines().filter_map(|line| {
+            let nums: Vec<usize> = line.split_whitespace()
+                .filter_map(|s| s.parse::<usize>().ok())
+                .collect();
+            if nums.len() == 3 {
+                Some((nums[0], (nums[1], nums[2])))
+            } else {
+                None
             }
-        }
-        location.push(source as usize);
-    }
-    // Get the min value
-    location.iter().min().unwrap().clone()
+        }).collect()
+    }).collect();
+
+    // Call the find_min_location function with the seeds and maps
+    find_min_location(&seeds, &maps)
 }
+
 pub fn part_2() -> usize {
     0
+}
+
+fn transform_number(number: usize, map: &HashMap<usize, (usize, usize)>) -> usize {
+    for (&dest_range_start, &(source_range_start, range_length)) in map {
+        if number >= source_range_start && number < source_range_start + range_length {
+            return number + dest_range_start - source_range_start;
+        }
+    }
+    number
+}
+
+fn find_min_location(seeds: &[usize], maps: &[HashMap<usize, (usize, usize)>]) -> usize {
+    seeds.iter().map(|&seed| {
+        maps.iter().fold(seed, |number, map| transform_number(number, map))
+    }).min().unwrap()
 }
